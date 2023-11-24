e7f5304fbc175809018525ee7a07f41f3dec0672
Dushmanta <dushmanta0511@gmail.com>
2023-11-24 14:59:24 +0530

    add Firebase to project, test data read from firebase

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import { useState } from "react";
+import Firebase from "./test-file/Firebase";
 
 function App() {
   return (
     <>
       <div>Welcome to Vroomride</div>
+      <Firebase />
     </>
   );
 }
