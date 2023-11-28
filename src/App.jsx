26b478cb1bcaf0f071f524d67e41cf01e39fad9d
Dushmanta <dushmanta.dev@gmail.com>
2023-11-28 12:18:41 +0530

    Add Bootstrap, write and update data test

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,9 @@ import Firebase from "./test-file/Firebase";
 function App() {
   return (
     <>
-      <div>Welcome to Vroomride</div>
+      <div className="container mt-3 mb-3 fs-3 fw-bold text-center">
+        Welcome to Vroomride
+      </div>
       <Firebase />
     </>
   );
--- src/test-file/Firebase.jsx
+++ src/test-file/Firebase.jsx
@@ -1,17 +1,59 @@
 import React from "react";
-import { getDatabase, ref, onValue } from "firebase/database";
+import { getDatabase, ref, onValue, set, update } from "firebase/database";
 import app from "../../firebase";
 
 const db = getDatabase(app);
-const starCountRef = ref(db, "bookings/");
-onValue(starCountRef, (snapshot) => {
-  const data = snapshot.val();
-  console.log(data);
-});
 
-console.log("Hello");
 const Firebase = () => {
-  return <div>Firebase</div>;
+  // Reading data from Firebase database
+
+  const starCountRef = ref(db, "bookings/");
+  onValue(starCountRef, (snapshot) => {
+    const data = snapshot.val();
+    console.log(data);
+  });
+
+  // Writing data to Firebase database
+
+  const carNum = "cars5";
+  const writeCarData = (carNum) => {
+    set(ref(db, "cars/" + carNum), {
+      carId: 5,
+      name: "Maruti",
+      availability: true,
+      price: 500,
+    });
+  };
+
+  // Update data in Firebase database
+
+  const updateCarNum = "cars4";
+  const updateCarData = (updateCarNum) => {
+    const carRef = ref(db, "cars/" + updateCarNum);
+    update(carRef, {
+      availability: false,
+    });
+  };
+
+  return (
+    <>
+      <div className="container">Firebase</div>
+      <div className="container mt-3">
+        <button
+          onClick={() => writeCarData(carNum)}
+          className="btn btn-outline-success m-3"
+        >
+          Add New Car
+        </button>
+        <button
+          onClick={() => updateCarData(updateCarNum)}
+          className="btn btn-outline-danger m-3"
+        >
+          Book your Car
+        </button>
+      </div>
+    </>
+  );
 };
 
 export default Firebase;
